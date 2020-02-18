70e100a43f8f928827d375bfe77f3b557ed1acf4
myfor <mrmyfor@outlook.com>
2020-02-18 12:52:12 +0800

    add: material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,41 @@
+import { NgModule, APP_INITIALIZER } from '@angular/core';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
+import { ThemeModule } from './theme/theme.module';
+import { RoutesModule } from './routes/routes.module';
 import { AppComponent } from './app.component';
 
+import { DefaultInterceptor } from '@core';
+import { StartupService } from '@core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+export function StartupServiceFactory(startupService: StartupService) {
+  return () => startupService.load();
+}
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    CoreModule,
+    SharedModule,
+    ThemeModule,
+    RoutesModule,
+    BrowserAnimationsModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: DefaultInterceptor, multi: true },
+    StartupService,
+    {
+      provide: APP_INITIALIZER,
+      useFactory: StartupServiceFactory,
+      deps: [StartupService],
+      multi: true,
+    },
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
