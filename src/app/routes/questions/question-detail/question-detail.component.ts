ad82b659807b30c1855eaf1c2b48664ae54c66ee
myfor <mrmyfor@outlook.com>
2020-03-20 09:23:06 +0800

    update: question detail

--- src/app/routes/questions/question-detail/question-detail.component.ts
+++ src/app/routes/questions/question-detail/question-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonService } from '../../../services/common.service';
+import { QuestionsService, QuestionDetail } from '../../../services/questions.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-question-detail',
@@ -7,9 +10,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuestionDetailComponent implements OnInit {
 
-  constructor() { }
+  id: number;
+  detail: QuestionDetail = {
+    title: 'title',
+    description: `
+    descpiptionaaaaaa
+    descpiptionaaaaaa
+    descpiptionaaaaaa
+    descpiptionaaaaaa
+    descpiptionaaaaaa
+    descpiptionaaaaaa
+    descpiptionaaaaaa
+    descpiptionaaaaaa
+    descpiptionaaaaaa
+    `,
+    state: 1,
+    createDate: '2020-02-02',
+    tags: ['a', 'b'],
+    votes: 120,
+    views: 100,
+    askerId: 1,
+    askerName: 'username',
+    askerThumbnail: 'assets/images/avatar.png',
+    answers: []
+  };
+
+  constructor(
+    private question: QuestionsService,
+    private route: ActivatedRoute,
+    private common: CommonService
+  ) { }
 
   ngOnInit() {
+    this.id = parseInt(this.route.snapshot.paramMap.get('id'), null);
+    this.getDetail();
   }
 
+  private getDetail() {
+    this.question.getQuestion(this.id)
+    .subscribe(r => {
+      if (r.isFault) {
+        this.common.snackOpen(r.message);
+        return;
+      } else {
+        this.detail = r.data;
+      }
+    });
+  }
 }
